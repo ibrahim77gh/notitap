17e2a97243dbf9485c9bbb8dffaa75094ded46ca
ibrahim77gh <syedibrahim4091@gmail.com>
2023-12-04 21:44:43 +0500

    Commit

--- src/Home.tsx
+++ src/Home.tsx
@@ -10,15 +10,13 @@ const Home = () => {
         <>
           <div className="outermost-container">
             <div className="flexing-container">
-              <div>
-                <Sidebar pages={allPages} />
-      
-                <div className="main-content">
-                  <div className="App container mx-auto px-16 flex flex-col gap-4 max-w-[100ch]">
-                    <main className="flex justify-start w-full">
-                      <Outlet/>
-                    </main>
-                  </div>
+              <Sidebar pages={allPages} />
+    
+              <div className="main-content">
+                <div className="App container mx-auto px-16 flex flex-col gap-4 max-w-[100ch]">
+                  <main className="flex justify-start w-full">
+                    <Outlet/>
+                  </main>
                 </div>
               </div>
             </div>
